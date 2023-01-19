1a8ac1e913952164a64a138195dc8d996051af73
choidahye <dhc0404@naver.com>
2023-01-19 22:26:50 +0900

    logout & loading

--- todo-react-app-ws/src/App.js
+++ todo-react-app-ws/src/App.js
@@ -2,23 +2,23 @@ import "./App.css";
 import Todo from "./Todo";
 import React from "react";
 import { useState, useEffect } from "react";
-import { Container, List, Paper } from "@mui/material";
+import { Container, List, Paper, Grid, Button, AppBar, Toolbar, Typography, } from "@mui/material";
 import AddTodo from "./AddTodo";
-import { call } from "./service/ApiService";
+import { call, signout } from "./service/ApiService";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    call("/todo", "GET", null)
-    .then((response) => setItems(response.data));
+    call("/todo", "GET", null).then((response) => {
+    setItems(response.data);
+    setLoading(false);
+    });
   }, []);
 
   const addItem = (item) => {
-    call("/todo", "POST", item)
-    .then((response) => {
-      setItems(response.data)}
-      );
+    call("/todo", "POST", item).then((response) => setItems(response.data))
   };
 
   const editItem = (item) => {
@@ -45,12 +45,49 @@ function App() {
       </List>
     </Paper>
   );
+
+    //네비게이션 바 추가
+    let navigationBar = (
+      <AppBar position="static">
+        <Toolbar>
+          <Grid justifyContent="space-between" container>
+            <Grid item>
+              <Typography variant="h6">오늘의 할 일</Typography>
+            </Grid>
+            <Grid item>
+              <Button color="inherit" raised onClick={ signout }>
+                로그아웃
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    )
+
+    //로딩중이 아닐 때 렌더링할 부분
+    let todoListPage = (
+      <div>
+        {navigationBar}
+        <Container maxWidth="md">
+          <AddTodo addItem={addItem}/>
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    )
+
+    //로딩중일 때 렌더링할 부분
+    let loadingPage = <h1>
+      로딩중...
+    </h1>
+    let content = loadingPage;
+
+    if(!loading){
+      content = todoListPage;
+    }
+
   return (
     <div className="App">
-      <Container maxWidth="md">
-        <AddTodo addItem={addItem} />
-        <div className="TodoList">{todoItems}</div>
-      </Container>
+      {content}
     </div>
   );
 }
